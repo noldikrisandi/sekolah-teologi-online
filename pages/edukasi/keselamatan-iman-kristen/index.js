05fc67c9fd9810d69e3e5037f3f993bd1a0904a9
noldikrisandi <krisandinoldi@gmail.com>
2025-01-25 21:01:53 +0800

    update sejarah bangsa israel, tapi belum selesai

--- pages/edukasi/keselamatan-iman-kristen/index.js
+++ pages/edukasi/keselamatan-iman-kristen/index.js
@@ -1,61 +1,331 @@
-import { Card } from "src/component/Card/Card";
+import Image from "next/image";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Edukasi() {
-  const cards = [
-    {
-      img: "/images/renungan-kristen.webp",
-      altText: "",
-      title: "Konsep Dasar Keselamatan",
-      desc: "Setiap orang percaya harus mengetahui Konsep Dasar Keselamatan dalam Iman Kristen, agar tidak salah dalam memahaminya...",
-      url: "/edukasi/keselamatan-iman-kristen/keselamatan-dalam-iman-kristen",
-      labelBtn: "Renungan",
-    },
-    {
-      img: "/images/renungan-rohani.webp",
-      altText:
-        "salib di padang rumput mejadi simbol pentingnya Menjadi Pribadi Penuh Damai",
-      title: "Syarat Memperoleh Keselamatan",
-      desc: "Didalam Alkitab dijelaskan bahwa untuk memperoleh keselamatan itu ada syarat dan konsekuensinya, inilah yang harus kita pahami, sehingga...",
-      url: "/edukasi/keselamatan-iman-kristen/hukum-kasih-kepada-tuhan",
-      labelBtn: "Renungan",
-    },
-    {
-      img: "/images/diberkati-tuhan.webp",
-      altText: "seseorang yang mendapatkan berkat kesejahteraan dari Tuhan",
-      title: "Kenapa Yesus harus Mati?",
-      desc: "Sebagian orang yang belum memahami konsep keselamatan dalam Kekristenan akan bertanya, kenapa sampai Yesus harus mati di kayu salib?...",
-      url: "/edukasi/keselamatan-iman-kristen/selalu-hidup-dalam-kesejahteraan",
-      labelBtn: "Renungan",
-    },
-    {
-      img: "/images/memuliakan-tuhan-dengan-memandang-salib.webp",
-      altText:
-        "sekumpulan orang yang memandang salib agar dapat memberi karena kasih",
-      title: "Bukan Sekadar Percaya",
-      desc: "Yesus hanya menyelamatkan orang yang percaya kepada-Nya. Percaya bukan hanya sekadar mengaku, tetapi harus melakukan perintah-Nya...",
-      url: "/edukasi/keselamatan-iman-kristen/memberi-karena-kasih",
-      labelBtn: "Renungan",
-    },
-  ];
-
   return (
     <>
-      <h1 className="text-2xl font-bold mb-6">
-        Edukasi Sekolah Teologi: Hukum Tuhan
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        {cards.map((card, index) => (
-          <Card
-            key={index}
-            img={card.img}
-            altText={card.altText}
-            title={card.title}
-            desc={card.desc}
-            url={card.url}
-            labelBtn={card.labelBtn}
-          />
-        ))}
-      </div>
+      <Head>
+        <title>Konsep Dasar Keselamatan dalam Iman Kristen</title>
+      </Head>
+      <header>
+        <div>
+          <h1 className="text-2xl py-4 font-semibold mt-5">
+            Konsep Dasar Keselamatan Dalam Iman Kristen (Kekristenan)
+          </h1>
+          <p className="text-md mb-4">Penulis: Noldi Krisandi</p>
+        </div>
+      </header>
+      <section>
+        <Image
+          src="/images/memuliakan-tuhan-dengan-memandang-salib.webp"
+          alt="sekumpulan orang yang memandang salib agar dapat memberi karena kasih"
+          className="w-full h-44 md:h-80 object-cover rounded mb-5"
+          width={500}
+          height={300}
+        />
+
+        <article>
+          <blockquote className="italic text-gray-950 mb-5">
+            &quot;Karena begitu besar kasih Allah akan dunia ini, sehingga Ia
+            telah mengaruniakan Anak-Nya yang tunggal, supaya setiap orang yang
+            percaya kepada-Nya tidak binasa, melainkan beroleh hidup yang
+            kekal.&quot;
+            <br />
+            <span className="block mt-2 text-sm">- Yohanes 3:16 </span>
+          </blockquote>
+        </article>
+
+        <article>
+          <h2 className="text-2xl font-semibold mt-5">
+            Pengertian Keselamatan
+          </h2>
+          <p>
+            <strong>Keselamatan adalah</strong> perlindungan keamanan dan
+            kenyamanan sehingga terbebas dari bahaya dan ancaman.
+          </p>
+          <p>
+            Konsep keselamatan dapat diterapkan dalam berbagai konteks, baik
+            dalam kehidupan fisik, mental, sosial, maupun spiritual.
+          </p>
+        </article>
+
+        <article>
+          <h2 className="text-2xl font-semibold mt-5">
+            Pengertian Keselamatan dalam Iman Kristen
+          </h2>
+          <p>
+            <strong>Keselamatan dalam iman Kristen adalah</strong> pembebasan
+            manusia dari hukuman dosa, sehingga dapat memperoleh kehidupan yang
+            kekal bersama Tuhan.
+          </p>
+          <p>
+            Keselamatan merupakan anugerah dari Tuhan yang diberikan kepada
+            manusia melalui iman kepada Yesus Kristus (Yohanes 3:16).
+          </p>
+        </article>
+        <article>
+          <h2 className="text-2xl font-semibold mt-5">
+            Latar Belakang Keselamatan Diberikan kepada Manusia
+          </h2>
+          <p>
+            Manusia diciptakan baik oleh Tuhan, namun karena dosa pertama yang
+            dilakukan oleh Adam dan Hawa, seluruh umat manusia jatuh dalam dosa
+            dan terpisah dari Tuhan. Sedangkan upah dosa adalah maut (Roma
+            6:23).
+          </p>
+          <p>
+            Semua orang adalah pendosa, dan tidak ada satu pun yang dapat
+            menyelamatkan dirinya sendiri dengan perbuatan baik atau usaha
+            pribadi (Roma 3:23).
+          </p>
+        </article>
+        <article>
+          <h2 className="text-2xl font-semibold mt-5">
+            Yesus Kristus Sebagai Jalan Keselamatan
+          </h2>
+          <p>
+            Keselamatan hanya dapat dicapai melalui Yesus Kristus. Sebagai Anak
+            Tuhan, Yesus datang ke dunia untuk mengorbankan diri-Nya di kayu
+            salib sebagai penebus dosa umat manusia. Pengorbanan-Nya ini
+            memungkinkan manusia untuk diperdamaikan dengan Tuhan.
+          </p>
+          <p>
+            Yesus adalah satu-satunya jalan menuju keselamatan (Yohanes 14:6),
+            dan hanya melalui iman kepada-Nya seseorang dapat memperoleh
+            keselamatan (Kisah Para Rasul 4:12).
+          </p>
+          Keselamatan yang diberikan oleh Tuhan mencakup beberapa aspek, yaitu:
+          <ol className="list-decimal pl-5">
+            <li>
+              <h3 className="font-semibold">
+                Penyelamatan dari Dosa dan Hukuman
+              </h3>
+              <p>
+                Keselamatan berarti pembebasan dari hukuman dosa yang seharusnya
+                diterima oleh setiap manusia. Dosa membawa maut (Roma 6:23),
+                tetapi melalui pengorbanan Yesus di kayu salib, manusia
+                diberikan kesempatan untuk dibebaskan dari hukuman tersebut
+                (Roma 5:8).
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">
+                Penyelamatan dari Kehidupan yang Terpisah dari Tuhan
+              </h3>
+              <p>Manusia yang berdosa terpisah dari Tuhan (Yesaya 59:2).</p>
+              <p>
+                Keselamatan dari Yesus memulihkan hubungan manusia dengan Tuhan,
+                sehingga memungkinkan manusia untuk hidup dalam persekutuan
+                dengan-Nya, baik di dunia ini maupun di kehidupan yang kekal.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">Hidup Kekal</h3>
+              <p>
+                Salah satu aspek utama keselamatan adalah janji hidup kekal
+                bersama Tuhan setelah kehidupan di dunia berakhir (Yohanes
+                3:16). Ini bukan hanya hidup yang panjang, tetapi hidup yang
+                penuh dengan kedamaian, sukacita, dan kemuliaan bersama Tuhan.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">Anugerah yang Cuma-Cuma</h3>
+              <p>
+                Keselamatan bukanlah sesuatu yang bisa diperoleh dengan usaha
+                atau perbuatan baik manusia, tetapi merupakan pemberian Tuhan
+                kepada orang yang percaya kepada Yesus (Efesus 2:8-9).
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">Transformasi Hidup</h3>
+              <p>
+                Setelah diselamatkan, orang percaya diubah secara rohani dan
+                dipanggil untuk hidup sesuai dengan kehendak Tuhan.
+              </p>
+              <p>
+                Keselamatan membawa dampak pada kehidupan sehari-hari, mengubah
+                cara hidup, pola pikir, dan hubungan dengan sesama.
+              </p>
+            </li>
+          </ol>
+        </article>
+        <article>
+          <h2 className="text-2xl font-semibold mt-5">
+            Kategori Orang yang Diselamatkan
+          </h2>
+          <p>
+            Tuhan hanya menyelamatkan orang yang percaya kepada Yesus. Setiap
+            orang bisa saja menganggap dirinya percaya kepada Yesus, tetapi hal
+            itu belum tentu benar.
+          </p>
+          <p>
+            Berikut ciri-ciri orang yang percaya kepada Yesus (Kategori Orang
+            yang Diselamatkan)
+          </p>
+          <ol className="list-decimal pl-5">
+            <li>
+              <h3 className="font-semibold">Mengenal Tuhan</h3>
+              <blockquote className="italic text-gray-950 mb-5">
+                &quot;Dan inilah tandanya, bahwa kita mengenal Allah, yaitu
+                jikalau kita menuruti perintah-perintah-Nya. Barangsiapa
+                berkata: ‘Aku mengenal Dia,’ tetapi ia tidak menuruti
+                perintah-Nya, ia adalah seorang pendusta dan di dalamnya tidak
+                ada kebenaran.&quot;
+                <br />
+                <span className="block mt-2 text-sm">- 1 Yohanes 2:3-4 </span>
+              </blockquote>
+              <p>
+                Kita tidak mungkin percaya kepada orang yang belum kita kenal,
+                begitu juga kita tidak mungkin percaya kepada Yesus jika tidak
+                mengenal-Nya.
+              </p>
+              <p>
+                1 Yohanes 2:3-4 dengan jelas menegaskan bahwa tanda orang
+                mengenal Yesus adalah melakukan perintah-Nya. Jika kita tidak
+                melakukan perintah Yesus artinya kita tidak mengenal Yesus.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">Mengasihi Yesus</h3>
+              <blockquote className="italic text-gray-950 mb-5">
+                &quot;Jikalau kamu mengasihi Aku, kamu akan menuruti segala
+                perintah-Ku.&quot;
+                <br />
+                <span className="block mt-2 text-sm">- Yohanes 14:15 </span>
+              </blockquote>
+              <p>
+                Yesus adalah kasih (1 Yohanes 4:8), Jika kita mengenal Yesus
+                artinya kita juga mengasihi Yesus.
+              </p>
+              <p>
+                Jika kita mengasihi Yesus, tentunya kita pasti menuruti segala
+                perintah-Nya
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">Mengalami Pertobatan</h3>
+              <blockquote className="italic text-gray-950 mb-5">
+                &quot;Karena itu sadarlah dan bertobatlah, supaya dosamu
+                dihapuskan.&quot;
+                <br />
+                <span className="block mt-2 text-sm">
+                  - Kisah Para Rasul 3:19
+                </span>
+              </blockquote>
+              <p>
+                Bertobat adalah berbalik sepenuhnya dari cara hidup yang tidak
+                berkenan kepada Allah, menjadi pribadi yang berkenan kepada
+                Allah.
+              </p>
+              <p>
+                Dalam Perjanjian Lama, konsep pertobatan sering kali
+                diterjemahkan dari kata Ibrani &quot;Shuv&quot;(שׁוּב) yang
+                berarti &quot;berbalik&quot; atau &quot;kembali&quot; kepada
+                Tuhan.
+              </p>
+              <p>
+                Kata Shuv muncul lebih dari 1.050 kali di dalam Perjanjian Lama,
+                tetapi tidak semuanya mengacu pada pertobatan secara rohani.
+                Sebagian digunakan untuk pengertian fisik (misalnya,
+                &quot;kembali ke tempat asal&quot;).
+              </p>
+              <p>
+                Namun, dalam konteks pertobatan atau kembalinya umat kepada
+                Tuhan, kata ini digunakan sekitar 100-120 kali, tergantung
+                terjemahan dan interpretasi.
+              </p>
+              <p>
+                Dalam Perjanjian Baru, kata bertobat diterjemahkan dari kata
+                Yunani &quot;Metanoeo&quot; (μετανοέω) yang berarti
+                &quot;mengubah pikiran&quot; atau &quot;berbalik dari
+                dosa.&quot;
+              </p>
+              <p>
+                Dalam teks asli Yunani Perjanjian Baru, kata
+                &quot;Metanoeo&quot; muncul sebanyak 34 kali. Variasi lain dari
+                kata ini, seperti &quot;Metanoia&quot; (pertobatan sebagai
+                konsep, bukan tindakan), muncul sebanyak 22 kali.
+              </p>
+              <p>
+                Jadi, total penggunaan istilah ini dalam konteks pertobatan
+                adalah sekitar 56 kali.
+              </p>
+              <p>
+                Pertobatan juga menjadi hal yang sering Tuhan Yesus sampaikan,
+                karena pertobatan menjadi hal yang sangat penting untuk
+                memperoleh keselamatan.
+              </p>
+              <p>
+                Setiap orang yang percaya kepada Yesus pasti menghasilkan buah
+                pertobatan. Jelas dikatakan dalam Kisah Para Rasul pasal ketiga,
+                bahwa bertobat menjadi salah satu syarat agar dosa kita
+                dihapuskan (memperoleh keselamatan).
+              </p>
+              <p>
+                Catatan pentingnya adalah keselamatan itu tidak diberikan karena
+                manusia bertobat, tapi karena percaya kepada Yesus. Setiap orang
+                yang percaya kepada Yesus pasti mengalami pertobatan.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-semibold">Melakukan Firman Tuhan</h3>
+              <blockquote className="italic text-gray-950 mb-5">
+                &quot;Bukan setiap orang yang berseru kepada-Ku: Tuhan, Tuhan!
+                akan masuk ke dalam Kerajaan Sorga, melainkan dia yang melakukan
+                kehendak Bapa-Ku yang di sorga.&quot;
+                <br />
+                <span className="block mt-2 text-sm">- Matius 7:21</span>
+              </blockquote>
+              <p>
+                Tujuan penyelamatan yang dilakukan oleh Yesus adalah melayakan
+                manusia untuk masuk ke dalam Kerajaan Sorga.
+              </p>
+              <p>
+                Matius 7:21 menyatakan syarat untuk mendapatkan keselamatan
+                (Masuk ke dalam Kerjaan Sorga) adalah dengan melakukan Kehendak
+                Tuhan (Firman Tuhan).
+              </p>
+              <p>
+                Melakukan Firman adalah syarat muthlak yang tidak bisa diganggu
+                gugat untuk memperoleh keselamatan.
+              </p>
+            </li>
+          </ol>
+        </article>
+        <article></article>
+        <h2 className="text-2xl font-semibold mt-5">
+          Jaminan Keselamatan dan Hidup Kekal
+        </h2>
+        <p>
+          Orang yang telah diselamatkan melalui iman kepada Yesus Kristus
+          memiliki jaminan hidup kekal. Ini berarti setiap orang yang
+          diselamatkan akan hidup bersama Tuhan selamanya, setelah kehidupan di
+          dunia ini berakhir (Yohanes 3:16).
+        </p>
+        <article>
+          <p>
+            Dapat disimpulkan bahwa Tuhan hanya menyelamatkan orang yang
+            berkenan kepada-Nya. Cara berkenan kepada Tuhan adalah dengan
+            melakukan Perintah-Nya dan tidak melakukan apa yang dilarang oleh
+            Tuhan.
+          </p>
+          <p>
+            Salah satu perintah Tuhan adalah percaya kepada Yesus. Kita tidak
+            mungkin berkenan dihadapan Tuhan jika tidak percaya kepada Yesus.
+          </p>
+          <p>
+            Kamu juga bisa membaca Renungan Cara Mendapatkan Keselamatan dari
+            Tuhan Yesus, agar lebih paham mengenai cara yang dapat dilakukan
+            untuk memperoleh keselamatan. Silahkan klik link di bawah ini.
+          </p>
+          <div className="w-full border-2 rounded py-3 pl-2 my-5 text-blue-600 font-medium">
+            <Link href="/renungan/mengenal-tuhan/mengasihi-sesama-manusia-seperti-diri-kita-sendiri">
+              Renungan Cara Mendapatkan Keselamatan dari Tuhan Yesus
+            </Link>
+          </div>
+        </article>
+      </section>
     </>
   );
 }
